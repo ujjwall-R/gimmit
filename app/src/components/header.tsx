73ad29b21c445512664121d28402e870374e3b3b
Aniket Raj <ani9431619703@gmail.com>
2025-01-30 01:43:26 +0530

    Commit-Analytics Widget

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -6,10 +6,25 @@ import DropDownSearchSelect from "./drop_down_search_select";
 
 export default function Header() {
   const [selectedRange, setSelectedRange] = useState<string | null>(null);
+  const menuItems = [
+    "archlinux/archwiki",
+    "microsoft/vscode",
+    "mongodb/mongo",
+    "facebook/react",
+    "python/cpython",
+    "torvalds/linux",
+    "flutter/flutter",
+    "vercel/next.js",
+    "golang/go",
+    "nodejs/node",
+    "facebook/create-react-app",
+    "microsoft/TypeScript",
+    "tailwindlabs/tailwindcss",
+  ];
 
   return (
-    <header className="bg-white shadow-md rounded-2xl border px-4 py-1 flex justify-between items-center h-16 mx-4 my-4">
-      <DropDownSearchSelect />
+    <header className="bg-white shadow-md rounded-2xl border px-4 py-1 flex justify-between items-center h-16 mx-4 mt-4">
+      <DropDownSearchSelect placeHolder={"Select a repository"} menuItems={menuItems}/>
       {/* <div className="flex items-center gap-4"> */}
         <DateRangePicker onRangeChange={setSelectedRange} />
         {/* <div className="w-10 h-10 rounded-full overflow-hidden border-2 border-gray-300">
